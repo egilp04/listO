82b556aa586f74944f4f7f576b3459f20764cea5
egilp04 <egilp04@iesalbarregas.es>
2026-02-05 17:40:42 +0100

    creacion del componente tabla

--- src/componentes/Table.tsx
+++ src/componentes/Table.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Button from "./Button";
 
 const Table = () => {
   const [info, setInfo] = useState<string[]>([]);
@@ -11,16 +12,24 @@ const Table = () => {
     getData();
   }, []);
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <label>Nombre:</label>
+    <div className="flex flex-col gap-6 max-h-100 overflow-auto  p-6">
+      <div className="flex flex-row w-full">
+        <label>Nombre</label>
       </div>
       <div className="flex flex-col">
         {info.map((inf, index) => (
           <div
-            className={`border border-neutral-50 ${index % 2 == 0 ? "bg-primary-100" : "bg-neutral-100"}`}
+            className={`items-center pt-4 pb-4 border border-neutral-50 flex flex-row ${index % 2 == 0 ? "bg-primary-100" : "bg-neutral-100"}`}
           >
-            <label>{inf.nombre}</label>
+            <label className="w-full font-bold">{inf.nombre}</label>
+            <div className="gap-4 flex flex-row justify-end pr-2">
+              <Button>
+                <span>Editar</span>
+              </Button>
+              <Button className="bg-danger-300">
+                <span className="text-black">Eliminar</span>
+              </Button>
+            </div>
           </div>
         ))}
       </div>
