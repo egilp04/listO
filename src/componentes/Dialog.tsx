3280d99034a01f4472fbec26b43b2f1966dcee2a
egilp04 <egilp04@iesalbarregas.es>
2026-02-05 21:03:15 +0100

    modificacion dle dialog (modal) para que tenga sombra

--- src/componentes/Dialog.tsx
+++ src/componentes/Dialog.tsx
@@ -7,7 +7,8 @@ interface DialogInterface {
 
 const Dialog = ({ titulo, descripcion }: DialogInterface) => {
   return (
-    <div className="flex flex-col gap-8 rounded-sm p-6 max-w-[500px]">
+    <div className="flex flex-col gap-8 rounded-sm p-6 max-w-125 bg-primary-50 shadow-elevation-3 border border-neutral-100">
+      <div className="flex w-full justify-end">x</div>
       <div className="flex flex-col gap-4 ">
         <h2>{titulo}</h2>
         <p>{descripcion}</p>
