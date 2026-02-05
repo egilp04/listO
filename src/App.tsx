982c060396bbb0e4a8121aa62967f67824bca3c9
Mery <mceballosm04@iesalbarregas.es>
2026-02-05 17:53:11 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
     <BrowserRouter>
       <div className="min-h-screen flex flex-col bg-white">
         <main className="flex-1 p-8">
-          <h1 className="text-2xl font-Titulos font-medium mb-4 text-black"> Footer</h1>
         </main>
 
         <Footer />
