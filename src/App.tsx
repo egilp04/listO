99e92f8671095c2b88d36018387cbcf2548f43f1
AlbertoKimi <breakingdawn1988@gmail.com>
2026-02-07 00:19:08 +0100

    Pruebas mias formularios

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,33 @@ import CardEstadisticaG from "./componentes/tarjetas/cardEstadisticaG";
 import { BrowserRouter } from 'react-router-dom'
 import Footer from './componentes/Footer'
 import Navbar from './componentes/Navbar'
+import { Genero } from "./componentes/Formularios/Genero";
+import { Login_ChangePasswd } from "./componentes/Formularios/Login_ChangePasswd";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col bg-white">
-        <header className="p-4 flex flex-col gap-4">
-          <Navbar />
 
-          <Navbar usuario="Mery" estaLogueado={true} />
+    <>
+    <Login_ChangePasswd login={true}></Login_ChangePasswd>
+    </>
+    // <BrowserRouter>
+    //   <div className="min-h-screen flex flex-col bg-white">
+    //     <header className="p-4 flex flex-col gap-4">
+    //       <Navbar />
 
-          <Navbar usuario="Admin" estaLogueado={true} esAdmin={true} />
-        </header>
+    //       <Navbar usuario="Mery" estaLogueado={true} />
 
-        <main className="flex-1 p-8 flex flex-col gap-8">
-          <h2 className="text-2xl font-bold mb-4">Demo CardEstadisticaG</h2>
-          <CardEstadisticaG numero={100} texto="Total Usuarios" />
-        </main>
+    //       <Navbar usuario="Admin" estaLogueado={true} esAdmin={true} />
+    //     </header>
 
-        <Footer />
-      </div>
-    </BrowserRouter>
+    //     <main className="flex-1 p-8 flex flex-col gap-8">
+    //       <h2 className="text-2xl font-bold mb-4">Demo CardEstadisticaG</h2>
+    //       <CardEstadisticaG numero={100} texto="Total Usuarios" />
+    //     </main>
+
+    //     <Footer />
+    //   </div>
+    // </BrowserRouter>
   )
 }
 
