ce7aa2a906b8a1f5042c58ac141e929ed673f22d
egilp04 <egilp04@iesalbarregas.es>
2026-02-07 14:12:38 +0100

    arreglos de la landing

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -20,10 +20,10 @@ const Landing = () => {
   return (
     <div className="min-h-screen flex flex-col bg-primary-50">
       <Navbar estaLogueado={false} />
-      <main className="grow flex flex-col items-center w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 gap-20">
+      <main className="grow flex flex-col items-center w-full mx-auto px-4 sm:px-6 lg:px-8 py-10 gap-20">
         {/* Sección Hero */}
         <section className="flex flex-col items-center text-center gap-8 w-full max-w-4xl">
-          <div className="space-y-4">
+          <div className="flex flex-col gap-4 md:gap-8">
             <h1>El espacio para tus historias</h1>
             <p>
               Centraliza todo tu entretenimiento. Gestiona libros, juegos, cine
@@ -58,8 +58,8 @@ const Landing = () => {
         {/* Carta 1 */}
         <section className="w-full">
           <CardLanding
-            titulo="Tu universo de entretenimiento"
-            descripcion="Olvida tener listas dispersas en notas o diferentes apps. Centraliza todos tus libros, videojuegos, películas y música en un único catálogo personal. ListO es el hogar digital para todas esas historias que has terminado y que merecen ser recordadas."
+            titulo="Tu catálogo personal"
+            descripcion="Olvida tener listas dispersas en notas o diferentes apps. Centraliza todo en un único catálogo personal. ListO es el hogar digital para todas esas historias que has terminado y que merecen ser recordadas."
             imagen={featureImage1}
             invertido={false}
           />
@@ -69,7 +69,7 @@ const Landing = () => {
         <section className="w-full">
           <CardLanding
             titulo="Tu crítica, tus reglas"
-            descripcion="No se trata solo de marcar como completado. Documenta tu experiencia: escribe una reseña personal, sube tu propia portada y asigna una valoración de 1 a 5 estrellas. Convierte cada ficha en una memoria imborrable de lo que ese título te hizo sentir."
+            descripcion="No se trata solo de marcar como completado. Documenta tu experiencia: escribe una reseña personal, sube tu propia portada y asigna una valoración de 1 a 5 estrellas. Convierte cada ficha en una memoria imborrable."
             imagen={featureImage2}
             invertido={true}
           />
@@ -79,7 +79,7 @@ const Landing = () => {
         <section className="w-full">
           <CardLanding
             titulo="Ordena tu caos digital"
-            descripcion="¿Buscas aquel RPG de hace tres años? Encuéntralo al instante. Utiliza nuestros filtros avanzados para organizar tu colección por género, plataforma, fecha de finalización o favoritos. Mantén tu biblioteca perfectamente estructurada sin esfuerzo."
+            descripcion="¿Buscas aquel RPG de hace tres años? Encuéntralo al instante. Utiliza nuestros filtros avanzados para matener tu biblioteca perfectamente estructurada sin esfuerzo."
             imagen={featureImage3}
             invertido={false}
           />
@@ -89,7 +89,7 @@ const Landing = () => {
         <section className="w-full">
           <CardLanding
             titulo="Visualiza tus logros"
-            descripcion="Descubre tus verdaderos hábitos de consumo cultural. Accede a tu panel de estadísticas para ver cuántos libros has leído este año, tus géneros más jugados y tu Top 3 de mejores experiencias. Deja que los datos cuenten tu historia."
+            descripcion="Descubre tus verdaderos hábitos de consumo cultural. Accede a tu panel de estadísticas y deja que los datos cuenten tu historia."
             imagen={featureImage4}
             invertido={true}
           />
@@ -98,8 +98,8 @@ const Landing = () => {
         {/* Sección Estadísticas */}
         <section className="w-full">
           <CardLanding
-            titulo="Miles de listas completadas con éxito"
-            descripcion="ListO no es solo una App, es la suma de vuestras ideas. Creada desde cero gracias a vuestras sugerencias."
+            titulo="Listas completadas"
+            descripcion="ListO no es solo una App, es la suma de tus ideas. Listas esperando a ser completadas por ti."
             imagen={statsImage}
             invertido={false}
           />
