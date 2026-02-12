02647b5d8a0746c53a60d089f17bbca89b7cdbe7
AlbertoV <breakingdawn1988@gmail.com>
2026-02-12 11:39:08 +0100

    Revert "logica de los inputs: select, checkbox y text area"

--- src/componentes/Inputs/Select.tsx
+++ src/componentes/Inputs/Select.tsx
@@ -1,4 +1,4 @@
-import { useState, type SelectHTMLAttributes } from "react";
+import type { SelectHTMLAttributes } from "react";
 
 export interface Option {
   value: string | number;
@@ -8,9 +8,6 @@ export interface Option {
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   options: Option[];
   variant?: "primario" | "info";
-  mensajeError: string;
-  manejarambio: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  manejarError: (nombre: string, error: boolean) => void;
 }
 
 export default function Select({
@@ -18,48 +15,26 @@ export default function Select({
   value = "",
   variant = "primario",
   disabled,
-  mensajeError,
-  manejarambio,
-  manejarError,
   ...props
 }: SelectProps) {
   const colorClass = `input-border-${variant}`;
 
-  const [error, setError] = useState(false);
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    manejarambio(e);
-    const valor = e.currentTarget.value;
-    const nombre = e.currentTarget.name;
-    if (valor == "") {
-      setError(true);
-      manejarError(nombre, true);
-    } else {
-      setError(false);
-      manejarError(nombre, false);
-    }
-  };
-
   return (
-    <div className="flex flex-col gap-0.5">
-      <select
-        value={value}
-        disabled={disabled}
-        className={`select-responsive input-style-comun ${disabled ? "input-disabled cursor-not-allowed" : `cursor-pointer ${"select-color-text"} ${colorClass}`}`}
-        {...props}
-        onChange={handleChange}
-      >
-        <option value="" disabled hidden>
-          Seleccionar
-        </option>
+    <select
+      value={value}
+      disabled={disabled}
+      className={`select-responsive input-style-comun ${disabled ? "input-disabled cursor-not-allowed" : `cursor-pointer ${"select-color-text"} ${colorClass}`}`}
+      {...props}
+    >
+      <option value="" disabled hidden>
+        Seleccionar
+      </option>
 
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-      {error && <span className="text-red-500">{mensajeError}</span>}
-    </div>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
   );
 }
--- src/componentes/Inputs/TextArea.tsx
+++ src/componentes/Inputs/TextArea.tsx
@@ -1,43 +1,19 @@
-import { useState, type TextareaHTMLAttributes } from "react";
+import type { TextareaHTMLAttributes } from "react";
 
 interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   label: string;
-  mensajeError?: string;
+  error?: string;
   variant?: "primario" | "info";
-  manejarCambio: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  manejarError: (nombre: string, error: boolean) => void;
 }
 
-function TextArea({
-  label,
-  mensajeError,
-  name,
-  disabled,
-  variant = "primario",
-  manejarCambio,
-  manejarError,
-  ...props
-}: TextAreaProps) {
-  const [error, setError] = useState(false);
+function TextArea({ label, error, name, disabled, variant = "primario", ...props }: TextAreaProps) {
 
   const colorClass = error ? "input-error" : `input-border-${variant}`;
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const nombre = e.currentTarget.name;
-    if (e.target.value.trim() == "") {
-      setError(true);
-      manejarError(nombre, true);
-    } else {
-      setError(false);
-      manejarError(nombre, false);
-    }
-    manejarCambio(e);
-  };
-
   return (
     <div className="flex flex-col gap-2 w-full">
-      <label
-        htmlFor={name}
+      <label 
+        htmlFor={name} 
         className={disabled ? "label-disabled" : "text-black"}
       >
         {label}
@@ -48,9 +24,12 @@ function TextArea({
         disabled={disabled}
         className={`input-style-comun textarea-style textarea-responsive ${disabled ? "input-disabled cursor-not-allowed" : `cursor-text  ${colorClass}`}`}
         {...props}
-        onChange={handleChange}
       />
-      {error && <span className="span-error">{mensajeError}</span>}
+      {error ? (
+        <p className="span-error mt-1 h-4">{error}</p>
+      ) : (
+        <div className="mt-1 h-4"></div>
+      )}
     </div>
   );
 }
