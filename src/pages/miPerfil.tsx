14500fc7d22073aaf7cde588311c41733e50737e
Mery <mceballosm04@iesalbarregas.es>
2026-02-13 20:34:38 +0100

    logica

--- src/pages/miPerfil.tsx
+++ src/pages/miPerfil.tsx
@@ -1,7 +1,60 @@
+import { useState } from "react";
 import Button from "../componentes/Button";
 import Inputs from "../componentes/Inputs/Inputs";
+import { supabase } from "../utils/supabaseClient";
 
 const MiPerfil = () => {
+
+  const [valores, setValores] = useState({
+    nombre: "",
+    email: "",
+    fechaNacimiento: "",
+  });
+
+  const [errores, setErrores] = useState({
+    nombre: false,
+    email: false,
+    fechaNacimiento: false,
+  });
+
+  const manejarCambio = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValores((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const manejarError = (nombre: string, error: boolean) => {
+    setErrores((prev) => ({ ...prev, [nombre]: error }));
+  };
+
+
+  const handleSubmit = async () => {
+    const hayErrores = Object.values(errores).some((e) => e);
+    const hayCamposVacios = Object.values(valores).some((v) => v === "");
+
+    if (hayErrores || hayCamposVacios) {
+      alert("Por favor, corrige los errores antes de continuar.");
+      return;
+    }
+
+    const { data, error } = await supabase.auth.updateUser({
+      email: valores.email,
+      options: {
+        data: {
+          nombre: valores.nombre,
+          fechanacimiento: valores.fechaNacimiento,
+        },
+      },
+    });
+
+    if (error) {
+      console.error("Error al actualizar:", error.message);
+      alert("Error al actualizar: " + error.message);
+    } else {
+      console.log("Datos actualizados:", data);
+      alert("Datos modificados correctamente.");
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <div className="grow w-full max-w-4xl mx-auto px-4 pb-8">
@@ -12,7 +65,16 @@ const MiPerfil = () => {
               <label className="font-bold">Nombre de usuario</label>
             </div>
             <div className="w-full md:w-2/3">
-              <Inputs label="" placeholder={"Nombre"} name="nombre" />
+              <Inputs
+                label=""
+                placeholder={"Nombre"}
+                name="nombre"
+                value={valores.nombre}
+                regex={/^[A-Z][a-zA-ZáéíóúÁÉÍÓÚñÑ ]{0,19}$/}
+                error="Debe empezar por mayúscula y máximo 20 caracteres"
+                manejarCambio={manejarCambio}
+                manejarError={manejarError}
+              />
             </div>
           </div>
           <div className="bg-white p-6 md:p-8 rounded-xl shadow-sm flex flex-col md:flex-row items-center gap-4">
@@ -25,6 +87,11 @@ const MiPerfil = () => {
                 placeholder={"correo@ejemplo.com"}
                 name="email"
                 type="email"
+                value={valores.email}
+                regex={/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/}
+                error="Introduce un email válido"
+                manejarCambio={manejarCambio}
+                manejarError={manejarError}
               />
             </div>
           </div>
@@ -34,18 +101,27 @@ const MiPerfil = () => {
               <label className="font-bold">Fecha Nacimiento</label>
             </div>
             <div className="w-full md:w-2/3">
-              <Inputs label="" name="fechaNacimiento" type="date" />
+              <Inputs
+                label=""
+                name="fechaNacimiento"
+                type="date"
+                value={valores.fechaNacimiento}
+                regex={/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/}
+                error="Formato válido: dd/mm/aaaa"
+                manejarCambio={manejarCambio}
+                manejarError={manejarError}
+              />
             </div>
           </div>
           <div className="flex justify-center">
             <div className="w-full md:w-1/3">
-              <Button variant="primario" className="w-full py-4 text-xl">
+              <Button variant="primario" className="w-full py-4 text-xl" onClick={handleSubmit}>
                 Modificar Datos
               </Button>
             </div>
           </div>
-        </div>{" "}
-      </div>{" "}
+        </div>
+      </div>
     </div>
   );
 };
