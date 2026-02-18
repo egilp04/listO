335b9042428ad322300463adeedcf3a81ba3c366
egilp04 <egilp04@iesalbarregas.es>
2026-02-18 13:22:04 +0100

    actualiacion del componente dialog

--- src/componentes/Dialog.tsx
+++ src/componentes/Dialog.tsx
@@ -3,9 +3,11 @@ import Button from "./Button";
 interface DialogInterface {
   titulo: string;
   descripcion: string;
+  imagen: string;
+  botones?: boolean;
 }
 
-const Dialog = ({ titulo, descripcion }: DialogInterface) => {
+const Dialog = ({ titulo, descripcion, imagen, botones }: DialogInterface) => {
   return (
     <div className="dialog">
       <div className="close-icon">
@@ -15,14 +17,21 @@ const Dialog = ({ titulo, descripcion }: DialogInterface) => {
         <h2>{titulo}</h2>
         <p>{descripcion}</p>
       </div>
-      <div className="flex flex-row gap-2 justify-end pr-2">
-        <Button variant="fantasma">
-          <span>No, cancelar</span>
-        </Button>
-        <Button>
-          <span>Sí, confirmar</span>
-        </Button>
-      </div>
+      {imagen && (
+        <div className="my-4 flex justify-center">
+          <img src={imagen} alt={titulo} className="max-h-40 object-contain" />
+        </div>
+      )}
+      {botones && (
+        <div className="flex flex-row gap-2 justify-end pr-2">
+          <Button variant="fantasma">
+            <span>No, cancelar</span>
+          </Button>
+          <Button>
+            <span>Sí, confirmar</span>
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
