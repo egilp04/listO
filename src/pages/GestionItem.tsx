6e5d2a18baa8108774683ff7459a66d1b1688427
egilp04 <egilp04@iesalbarregas.es>
2026-02-21 12:18:17 +0100

    cambios modo oscuro file y biblioteca añadir item

--- src/pages/GestionItem.tsx
+++ src/pages/GestionItem.tsx
@@ -23,30 +23,31 @@ const GestionItem: React.FC<GestionItemProps> = ({ item }) => {
   const manejarCambio = (e: any) => {
     setForm({
       ...form,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
     console.log(e.target.value);
   };
 
   const cabeceraGestion =
-    "bg-primary-600 p-3 flex justify-center items-center relative cursor-pointer text-white hover:bg-primary-700 transition-colors";
+    "bg-primary-600 dark:bg-primary-850 p-3 flex justify-center items-center relative cursor-pointer text-white hover:bg-primary-700 transition-colors";
 
   return (
-    <div className="min-h-screen bg-primary-200 p-4 flex flex-col gap-6">
+    <div className="min-h-screen bg-primary-200 dark:bg-primary-1000 p-4 flex flex-col gap-6">
       <h2 className="font-bold mb-10 text-center">Añadir una nueva ficha</h2>
       <div className="flex flex-col md:flex-row gap-6">
-        <div className="bg-white rounded-xl p-3 flex flex-col sm:flex-row items-center justify-between w-full md:w-1/2 shadow-sm min-h-16 gap-3">
-          <span>IMAGEN: </span>
+        <div className="bg-white dark:bg-primary-900 rounded-xl p-3 flex flex-col sm:flex-row items-center justify-between w-full md:w-1/2 shadow-sm min-h-16 gap-3">
+          <span className="dark:text-primary-50">IMAGEN: </span>
           <File
             label="Subir"
             name="imagen"
             onChange={manejarCambio}
             disabled={false}
-            mensajeError="No es un formato valido(.png, .jpg, .webp)" />
+            mensajeError="No es un formato valido(.png, .jpg, .webp)"
+          />
         </div>
 
-        <div className="bg-white rounded-xl p-3 flex items-center justify-between w-full md:w-1/2 shadow-sm min-h-16">
-          <span>Valoración:</span>
+        <div className="bg-white dark:bg-primary-900 rounded-xl p-3 flex items-center justify-between w-full md:w-1/2 shadow-sm min-h-16">
+          <span className="dark:text-primary-50">Valoración:</span>
           <div className="flex gap-1">
             {[1, 2, 3, 4, 5].map((star) => (
               <span
@@ -61,14 +62,16 @@ const GestionItem: React.FC<GestionItemProps> = ({ item }) => {
         </div>
       </div>
 
-      <div className="bg-white rounded-xl p-4 shadow-sm flex flex-col md:flex-row items-center gap-4">
-        <span className="whitespace-nowrap">INFORMACIÓN GENERAL:</span>
+      <div className="bg-white dark:bg-primary-900 rounded-xl p-4 shadow-sm flex flex-col md:flex-row items-center gap-4">
+        <span className="whitespace-nowrap dark:text-primary-50 ">
+          INFORMACIÓN GENERAL:
+        </span>
         <Inputs />
       </div>
 
-      <div className="bg-white rounded-xl overflow-hidden shadow-sm">
+      <div className="bg-white dark:bg-primary-900 rounded-xl overflow-hidden shadow-sm">
         <div className={cabeceraGestion}>
-          <span>Tipo</span>
+          <span className="dark:text-primary-50">Tipo</span>
           <span className="material-symbols-outlined absolute right-4">
             expand_more
           </span>{" "}
@@ -77,7 +80,7 @@ const GestionItem: React.FC<GestionItemProps> = ({ item }) => {
           {["Libro", "Juego"].map((tipo) => (
             <div
               key={tipo}
-              className="bg-primary-600 rounded-lg p-2 w-full md:w-5/12 flex justify-center"
+              className="bg-primary-600 dark:bg-primary-850 rounded-lg p-2 w-full md:w-5/12 flex justify-center"
             >
               <Checkbox label={tipo} colorTexto="text-white" />
             </div>
@@ -85,7 +88,7 @@ const GestionItem: React.FC<GestionItemProps> = ({ item }) => {
         </div>
       </div>
 
-      <div className="bg-white rounded-xl overflow-hidden shadow-sm">
+      <div className="bg-white dark:bg-primary-900 rounded-xl overflow-hidden shadow-sm">
         <div className={cabeceraGestion}>
           <span>Genero</span>
           <span className="material-symbols-outlined absolute right-4">
@@ -97,9 +100,9 @@ const GestionItem: React.FC<GestionItemProps> = ({ item }) => {
           {["Suspense", "Miedo"].map((genero) => (
             <div
               key={genero}
-              className="flex items-center gap-4 bg-blue-50/50 p-2 rounded-lg"
+              className="flex items-center gap-4  p-2 rounded-lg"
             >
-              <div className="bg-primary-600 text-white px-8 py-1 rounded-md text-lg min-w-150 text-center">
+              <div className="bg-primary-600 dark:bg-primary-850 text-white px-8 py-1 rounded-md text-lg min-w-150 text-center">
                 {genero}
               </div>
               <Checkbox label="Descripción" />
