ed940f788b358c2f6bdf296ff8622ed9df82fcdf
Mery <mceballosm04@iesalbarregas.es>
2026-03-04 18:36:38 +0100

    tiempo carga

--- src/App.tsx
+++ src/App.tsx
@@ -85,10 +85,9 @@ function App() {
   useEffect(() => {
     initialize();
 
-    // Forzamos un tiempo mínimo de carga de 1 segundos
     const timer = setTimeout(() => {
       setMinLoadingTime(false);
-    }, 1000);
+    }, 500);
 
     const handleVisibilityChange = () => {
       if (document.visibilityState === "visible") {
